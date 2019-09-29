d1c7e88a38644ba780d6d9f57ea892a7c697e648
elijahnahalala <55806295+elijahnahalala@users.noreply.github.com>
2019-09-28 18:53:44 -0700

    made home page

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+}
+
+$('.dropdown-toggle').click(function(e) {
+  if ($(document).width() > 768) {
+    e.preventDefault();
+
+    var url = $(this).attr('href');
+
+    if (url !== '#') {
+      window.location.href = url;
+    }
+
+  }
+});
